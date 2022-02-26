a9f668d5879ae0b69250a113164d8125ebe97a5f
Wilman Ortiz Navarro <68874713+wortiz1027@users.noreply.github.com>
2022-02-26 18:22:30 -0500

    Delete checkout.groovy

--- vars/checkout.groovy
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env groovy
-
-def call(String inBranch = "master", String inRepository, String inCredentials) {
-  git branch: inBranch, url: inRepository, credentialsId: inCredentials
-}

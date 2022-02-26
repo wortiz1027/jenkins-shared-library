f9fcb9b2165aa7754aba3b9f5bb6c9f3c8f26a88
Wilman Ortiz Navarro <68874713+wortiz1027@users.noreply.github.com>
2022-02-26 17:29:17 -0500

    Update GitUtils.groovy

--- src/io/developer/GitUtils.groovy
+++ src/io/developer/GitUtils.groovy
@@ -1,7 +1,7 @@
 package io.developer
 
-def checkOut(inBranch = "master", repository) {
-  git branch: inBranch, url: repository
+def checkOut(inBranch = "master", repository, credentials) {
+  git branch: inBranch, url: repository, credentialsId: credentials
 }
 
 return this
